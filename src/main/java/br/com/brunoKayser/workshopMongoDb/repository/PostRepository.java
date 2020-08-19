7da05ef729cef67c73974f933a25b98ced5a2081
BrunoKayser <bruno.kaysers@gmail.com>
2020-08-18 22:35:34 -0300

    Consulta com vários critérios

--- src/main/java/br/com/brunoKayser/workshopMongoDb/repository/PostRepository.java
+++ src/main/java/br/com/brunoKayser/workshopMongoDb/repository/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -18,5 +19,9 @@ public interface PostRepository extends MongoRepository<Post, String> {
     //Pesquisa por método, o método faz consulta ao banco
     //O nome do método importa
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    //@Query("{ $and: [ { date: {$gte: ?1 } }, { date: { $lte: ?2 } } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    @Query("{ $and: [ { date: {$gte: ?1 } }, { date: { $lte: ?2 } } , { $or: [ { 'title': {$regex: ?0, $options: i } },  {'body': {$regex: ?0, $options: i } } , {'comments.text': {$regex: ?0, $options: i } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
--- src/main/java/br/com/brunoKayser/workshopMongoDb/resources/PostResource.java
+++ src/main/java/br/com/brunoKayser/workshopMongoDb/resources/PostResource.java
@@ -1,14 +1,13 @@
 package br.com.brunoKayser.workshopMongoDb.resources;
 
 import br.com.brunoKayser.workshopMongoDb.domain.Post;
-import br.com.brunoKayser.workshopMongoDb.domain.User;
-import br.com.brunoKayser.workshopMongoDb.dto.UserOptionalDTO;
 import br.com.brunoKayser.workshopMongoDb.resources.util.URL;
 import br.com.brunoKayser.workshopMongoDb.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,6 +30,18 @@ public class PostResource {
 
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate,new Date(0L));
+        Date max = URL.convertDate(maxDate,new Date());
+        List<Post> list = postService.fullSearch(text,min,max);
         return ResponseEntity.ok().body(list);
     }
 }
